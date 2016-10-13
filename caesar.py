1fcfd2e416f47b0be5579021093b7af5136b9a20
TaQini <742954809@qq.com>
2016-10-13 14:18:33 +0800

    add list rot0-25

--- caesar.py
+++ caesar.py
@@ -5,35 +5,55 @@
 
 import sys
 
-try:
-	if len(sys.argv[1:]) == 2:
-		s = sys.argv[1]
-		x = int(sys.argv[2])
-	else:
-		s = raw_input("String:\r\n")
-		x = int(raw_input("offset:\r\n"))
-except ValueError:
-	print "[!]ValueErrorr\r\n"
-	exit(0)
+s = ""
+x = 13
 
-res = ""
+def usage():
+    print "Usage: " + str(sys.argv[0]) + "string offset/list"
+    
+def get():
+    global s
+    global x
+    if len(sys.argv[1:]) != 0:
+        s = sys.argv[1]
+        x = sys.argv[2]		
+    else :
+        s = raw_input("String:\r\n")
+        x = raw_input("offset:\r\n")    
+def caesar(s,x):
+    res = ""		
+    for c in s:
+        d = ord(c) + int(x)
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            if d > ord('z'):
+                d -= 26
+            elif d < ord('a'):
+                d += 26
+            res += chr(d)
+        elif ord(c) >= ord('A') and ord(c) <= ord('Z'):
+            if d > ord('Z'):
+                d -= 26
+            elif d < ord('a'):
+                d += 26
+            res += chr(d)
+        else:
+            res += c
+    return res
 
-for c in s:
-	d = ord(c) + x
-	if ord(c) >= ord('a') and ord(c) <= ord('z'):
-		if d > ord('z'):
-			d -= 26
-		elif d < ord('a'):
-			d += 26
-		res += chr(d)
-	elif ord(c) >= ord('A') and ord(c) <= ord('Z'):
-		if d > ord('Z'):
-			d -= 26
-		elif d < ord('a'):
-			d += 26
-		res += chr(d)
-	else:
-		res += c
-print res
+def ls(s):
+    for i in range(0,26):
+        print "rot" + str(i) + ": " + caesar(s,str(i))
 
+def main():
+    get()    
+    try:
+        if x == "l":
+            ls(s)
+        else:
+            print caesar(s,x)		
+    except ValueError:
+        print "[!]ValueErrorr\r\n"
+        exit(0)    
+        
+main()
 # abcdefghijklmnopqrstuvwxyz
